88a7c628372106b4ad47db14d305ae8ef699690d
Coffee <alanjimenes2006@gmail.com>
2026-04-02 15:23:50 -0400

    diseño pt 2

--- src/main/java/com/example/algoritmosclasicosproyecto/controladores/MenuController.java
+++ src/main/java/com/example/algoritmosclasicosproyecto/controladores/MenuController.java
@@ -4,55 +4,58 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 
-import javax.swing.text.AbstractDocument;
 import java.util.Objects;
 
 public class MenuController {
+
     @FXML
     public Button btnInicio, btnMapa, btnRutas, btnParadas;
 
     @FXML
     public BorderPane pnlContenedor;
 
+    @FXML
     public void btnInicioClick() {
         estilizarBotones(btnInicio);
         pnlContenedor.getChildren().clear();
     }
-    public void btnMapaClick(){
+
+    @FXML
+    public void btnMapaClick() {
         estilizarBotones(btnMapa);
         cambiarPanel("mapa");
     }
 
-    public void btnRutasClick(){
+    @FXML
+    public void btnRutasClick() {
         estilizarBotones(btnRutas);
         cambiarPanel("rutas");
-
     }
 
-    public void btnParadasClick(){
+    @FXML
+    public void btnParadasClick() {
         estilizarBotones(btnParadas);
         cambiarPanel("paradas");
     }
 
-    public void estilizarBotones(Button btn){
+    public void estilizarBotones(Button btn) {
         String estiloBase = "-fx-background-color: transparent; -fx-text-fill: white;";
-
         String estiloClick = "-fx-background-color: #3F326D; -fx-text-fill: white;";
+
+        btnInicio.setStyle(btn == btnInicio ? estiloClick : estiloBase);
         btnMapa.setStyle(btn == btnMapa ? estiloClick : estiloBase);
         btnRutas.setStyle(btn == btnRutas ? estiloClick : estiloBase);
         btnParadas.setStyle(btn == btnParadas ? estiloClick : estiloBase);
     }
 
-    public void cambiarPanel(String nombre){
-        try{
+    public void cambiarPanel(String nombre) {
+        try {
             AnchorPane contenido = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/algoritmosclasicosproyecto/" + nombre + "-view.fxml")));
             pnlContenedor.getChildren().clear();
             pnlContenedor.setCenter(contenido);
-        }
-        catch (Exception ignored){
+        } catch (Exception ignored) {
             ignored.printStackTrace();
         }
     }
